e46ef6691ab16d9762f6672d22e7fa9efe23c831
Jhonatas Jardim <jhonatasjardim@hotmail.com>
2021-01-13 12:19:18 -0300

    Asynchronous operations using Tasks (async, await)

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -29,8 +29,7 @@ namespace SalesWebMvc.Models
         [Display(Name = "Base Salary")]
         [DisplayFormat(DataFormatString = "{0:F2}")]
         public double BaseSalary { get; set; }
-
-        [Required(ErrorMessage = "{0} required")]
+        
         public Department Department { get; set; }
         public int DepartmentId { get; set; }
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
